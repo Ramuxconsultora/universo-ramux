625aabcc78289f912b4091ea045bf212714ae850
Ramuxconsultora <holaramux@gmail.com>
2026-03-13 16:37:51 -0300

    Update main.jsx

--- modern-dashboard/src/main.jsx
+++ modern-dashboard/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n'
-import App from './App.jsx'
+// Importamos con 'A' mayúscula para coincidir con tu archivo físico
+import App from './App.jsx' 
 import { AuthProvider } from './contexts/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
